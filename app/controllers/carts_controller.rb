c91341eb7c23bcc5f411880f54d15c03c411d67c
Jekson365 <jemali.ergemlidze.1@btu.edu.ge>
2024-08-28 22:03:23 +0400

    cc

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,18 +10,19 @@ class CartsController < ApplicationController
 
   def cart_items
     user_id = cart_index_params[:user_id]
-    query = "
-           SELECT  products.title,users.email,products.id as product_id,price,product_images.id as image_id,
-        CONCAT('/uploads/product_image/image/',
-        product_images.id,'/',product_images.image) as image_url,
-        COUNT(products.id) as product_count FROM carts
-        LEFT JOIN products ON carts.product_id = products.id
-        LEFT JOIN users ON carts.user_id = users.id
-        LEFT JOIN product_images ON products.id = product_images.product_id
-        WHERE users.id = #{user_id} AND product_images.main = true
-        GROUP BY products.id,products.title,users.email,price,image_url,product_images.id
-          "
-    result = ActiveRecord::Base.connection.execute(query)
+
+    query = <<-SQL
+    SELECT products.title, users.email, products.id as product_id, price, product_images.id as image_id,
+    CONCAT('/uploads/product_image/image/', product_images.id, '/', product_images.image) as image_url,
+    COUNT(products.id) as product_count FROM carts
+    LEFT JOIN products ON carts.product_id = products.id
+    LEFT JOIN users ON carts.user_id = users.id
+    LEFT JOIN product_images ON products.id = product_images.product_id
+    WHERE users.id = ? AND product_images.main = true
+    GROUP BY products.id, products.title, users.email, price, image_url, product_images.id
+  SQL
+
+    result = ActiveRecord::Base.connection.execute(ActiveRecord::Base.send(:sanitize_sql_array, [query, user_id]))
 
     render json: result
   end
