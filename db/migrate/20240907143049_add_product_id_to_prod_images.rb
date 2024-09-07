8832e157a9eb8e518c3adc5e695496951e349266
Jekson365 <jemali.ergemlidze.1@btu.edu.ge>
2024-09-07 18:36:16 +0400

    cc

--- db/migrate/20240907143049_add_product_id_to_prod_images.rb
+++ db/migrate/20240907143049_add_product_id_to_prod_images.rb
@@ -1,5 +1,6 @@
 class AddProductIdToProdImages < ActiveRecord::Migration[7.1]
   def change
-    add_reference :product_images,:product,foreign_key: true
+    add_reference :product_images, :product, foreign_key: true
+    remove_column :product_images, :about_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -125,11 +125,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_07_143049) do
   end
 
   create_table "product_images", force: :cascade do |t|
-    t.bigint "about_id", null: false
     t.string "image"
     t.boolean "main", default: false
     t.bigint "product_id"
-    t.index ["about_id"], name: "index_product_images_on_about_id"
     t.index ["product_id"], name: "index_product_images_on_product_id"
   end
 
@@ -190,7 +188,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_07_143049) do
   add_foreign_key "carts", "users"
   add_foreign_key "featured_products", "products"
   add_foreign_key "opening_images", "openings"
-  add_foreign_key "product_images", "abouts"
   add_foreign_key "product_images", "products"
   add_foreign_key "products", "categories"
 end
